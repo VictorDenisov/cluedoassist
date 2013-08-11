8934d9312b7875c705f541cef6a33d2509bc72de
Victor Denisov <denisovenator@gmail.com>
2013-08-11 15:43:38 +0400

    Initial import.

---
+++ src/main/java/com/cluedoassist/App.java
@@ -0,0 +1,13 @@
+package com.cluedoassist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
