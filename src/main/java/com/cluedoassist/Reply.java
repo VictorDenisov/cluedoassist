f37a2fa16ca0b5dcd1d4f8400a60a76480153e1f
Victor Denisov <denisovenator@gmail.com>
2013-09-09 01:56:54 +0400

    Reply fields are made public.

--- src/main/java/com/cluedoassist/Reply.java
+++ src/main/java/com/cluedoassist/Reply.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 
 public final class Reply implements Serializable {
 
-    final String replier;
+    public final String replier;
 
-    final CardReply cardReply;
+    public final CardReply cardReply;
 
     public Reply(String replier, CardReply cardReply) {
         this.replier = replier;
