c0dbf69a3d7b6206bc60eebab99dae562652a74f
Victor Denisov <denisovenator@gmail.com>
2013-10-05 13:13:08 +0400

    Added error checks to Accusation.

--- src/main/java/com/cluedoassist/Accusation.java
+++ src/main/java/com/cluedoassist/Accusation.java
@@ -13,6 +13,16 @@ public final class Accusation implements LogEntry {
     public final List<Card> cards;
 
     public Accusation(String asker, List<Card> cards) {
+        if (asker == null || "".equals(asker)) {
+            throw new IllegalArgumentException("asker can't be null or empty");
+        }
+        if (cards == null) {
+            throw new IllegalArgumentException("cards can't be null");
+        }
+        if (cards.size() != 3) {
+            throw new IllegalArgumentException(
+                                        "there should be exactly three cards");
+        }
         this.asker = asker;
 
         this.cards = Collections.unmodifiableList(new ArrayList<Card>(cards));
