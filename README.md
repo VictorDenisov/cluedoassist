f89b23a7b53e30423701354af49e2f45294f7e57
Victor Denisov <denisovenator@gmail.com>
2013-10-19 11:35:39 +0400

    Legal information is added.

---
+++ README.md
@@ -0,0 +1,5 @@
+Cluedo Assistant
+================
+
+This is a domain library for Cluedo board game. This library allows you to
+make inferences about known cards using game log.
--- src/main/java/com/cluedoassist/UnknownCardException.java
+++ src/main/java/com/cluedoassist/UnknownCardException.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2013 Victor Denisov
+ *
+ * This file is part of Cluedo Assistant.
+ *
+ * Cluedo Assistant is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cluedo Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cluedo Assistant.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.cluedoassist;
 
 public class UnknownCardException extends Exception {
